0176576bf911fed8a28361af4a7f6d6cbc218a3d
czk <ihadyou@qq.com>
2025-12-03 14:51:10 +0800

    chore: 更新版本号至0.0.7

--- fastofd/__init__.py
+++ fastofd/__init__.py
@@ -4,7 +4,7 @@ from importlib.metadata import version, PackageNotFoundError
 try:
     __version__ = version("fastofd")
 except PackageNotFoundError:
-    __version__ = "0.0.6"
+    __version__ = "0.0.7"
 __author__ = "ihadyou"
 __email__ = "wohen@nivbi.com"
 __description__ = "一个用于OFD文档处理的Python库"
